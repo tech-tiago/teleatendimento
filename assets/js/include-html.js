bee1d0f8d2043bb4239b59a61515cf3b6d2ac58e
tech-tiago <gsfogo@gmail.com>
2023-10-19 18:08:30 -0300

    3.0

---
+++ assets/js/include-html.js
@@ -0,0 +1,20 @@
+function includeHTML(file, targetId) {
+    fetch('./../../tipificacoes/' + file) // Caminho relativo para a pasta 'tipificações'
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById(targetId).innerHTML = html;
+        })
+        .catch(error => {
+            console.error('Erro ao carregar o arquivo HTML:', error);
+        });
+}
+
+// Exemplo de uso:
+includeHTML('tipificacoes.html', 'tipificacoes');
+includeHTML('abandono-incapaz.html', 'abandonoIncapaz');
+includeHTML('acidentes-com-vitimas.html', 'acidentesComVitimas');
+includeHTML('acidentes-sem-vitimas.html', 'acidentesSemVitimas');
+includeHTML('afogamento.html', 'afogamento');
+includeHTML('agradecimento.html', 'agradecimento');
+
+
